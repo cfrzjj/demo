a906a7625fa98898978bc799e55a3c992802b64f
cfrzjj <38045008+cfrzjj@users.noreply.github.com>
2022-02-24 17:39:31 +0800

    Update test.java

--- src/test/java/xiancheng/test.java
+++ src/test/java/xiancheng/test.java
@@ -13,6 +13,7 @@ public class test {
         muityThread muityThread = new muityThread();
         for (int i = 0; i < count; i++) {
             fixedThreadPool.execute(muityThread);
+            int aaa = 3;
         }
     }
 }
